8e7ee7ce4850fcd395da81c7e08b9f5770c66d25
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-02-06 01:16:24 +0300

    filterChain

--- src/main/java/com/naumovets/spring_secutiry/configs/SecurityConfig.java
+++ src/main/java/com/naumovets/spring_secutiry/configs/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.naumovets.spring_secutiry.configs;
 
+import com.naumovets.spring_secutiry.service.authentication.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.type.StandardAnnotationMetadata;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -20,44 +21,39 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 @Slf4j
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
     private final JwtRequestFilter jwtRequestFilter;
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        log.info("Dao Authentication Provider");
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/api/v1/admin").hasRole("ADMIN")
-//                .antMatchers("/api/v1/profile").authenticated()
-//                .antMatchers("/h2-console/**").permitAll()
-//                .anyRequest().permitAll()
-//                .and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .headers().frameOptions().disable()
-//                .and()
-//                .exceptionHandling()
-//                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-//
-//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("Dao Authentication Provider");
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/admin").hasRole("ADMIN")
+                .antMatchers("/api/v1/profile").authenticated()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Override
     @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationProvider... providers) {
-        return new ProviderManager(providers);
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
-
-//    @Bean
-//    public AuthenticationProvider authenticationProviderBean() {
-//        return new StandardAuthenticationProvider();
-//    }
-
 }
