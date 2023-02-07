0e9f0729c43642d95f3e3881956b2e51f4ca4af3
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-02-08 02:57:04 +0300

    cart

--- src/main/java/com/naumovets/spring_secutiry/cart/Cart.java
+++ src/main/java/com/naumovets/spring_secutiry/cart/Cart.java
@@ -1,32 +1,60 @@
 package com.naumovets.spring_secutiry.cart;
 
 import com.naumovets.spring_secutiry.entities.products.Product;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import com.naumovets.spring_secutiry.exceptions.ResourceNotFoundException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@Component
-@Scope("singleton")
+@Slf4j
 public class Cart {
-    private List<Product> list;
+    private List<CartItem> list;
+    @Getter
+    private int totalPrice;
 
-    @PostConstruct
-    public void setList() {
+    public Cart() {
         list = new ArrayList<>();
     }
 
-    public List<Product> getProducts() {
-        return list;
+    public List<CartItem> getItems() {
+        return Collections.unmodifiableList(list);
     }
 
-    public void addProduct(Product product) {
-        list.add(product);
+    public void add(Product product) {
+        CartItem item = list.stream().filter(cartItem -> cartItem.getProductId().equals(product.getId())).findFirst().orElse(null);
+        if (item == null) {
+            list.add(new CartItem(product.getId(), product.getTitle(), 1, product.getCost(), product.getCost()));
+        } else {
+            item.setQuantity(item.getQuantity() + 1);
+            item.setPrice(item.getPricePerProduct() * item.getQuantity());
+        }
+        recalculate();
     }
 
-    public void deleteById(Product product) {
-        list.remove(product);
+    public void delete(Product product) {
+        list.removeIf(cartItem -> cartItem.getProductId().equals(product.getId()));
+        recalculate();
+    }
+
+    private void recalculate() {
+        totalPrice = 0;
+        list.forEach(cartItem -> totalPrice = totalPrice + cartItem.getPrice());
+        log.info(String.valueOf(totalPrice));
+    }
+
+    public void changeQuantity(Long id, Integer delta) {
+        CartItem item = list.stream().filter(cartItem -> cartItem.getProductId().equals(id)).findFirst().orElseThrow(() -> new ResourceNotFoundException("Cart item not found, id: " + id));
+        if (item.getQuantity() == 0 && delta == -1) {
+            return;
+        } else {
+            item.setQuantity(item.getQuantity() + delta);
+            item.setPrice(item.getPricePerProduct() * item.getQuantity());
+            recalculate();
+        }
     }
 }
+
+
---
+++ src/main/java/com/naumovets/spring_secutiry/cart/CartItem.java
@@ -0,0 +1,16 @@
+package com.naumovets.spring_secutiry.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+    private Long productId;
+    private String productTitle;
+    private int quantity;
+    private int pricePerProduct;
+    private int price;
+}
