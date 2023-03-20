932fdd55dc80284375d6607e0fc6d0afc1ea5e89
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-03-20 21:00:12 +0300

    Spring practice_hw_11. Add alert for exception when registration.

--- fr-service/src/main/resources/static/registration/registration.js
+++ fr-service/src/main/resources/static/registration/registration.js
@@ -2,15 +2,20 @@ angular.module('market').controller('registrationController', function ($scope, 
     const contextPath = 'http://localhost:8193/';
 
     $scope.functionRegistration = function () {
-        $http.post(contextPath + 'auth/registration', $scope.reguser).then(function (response) {
-            if (response.data.token) {
-                $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
-                $localStorage.springWebUser = {username: $scope.reguser.username, token: response.data.token, isAdmin: response.data.isAdmin};
+        $http.post(contextPath + 'auth/registration', $scope.reguser)
+            .then(function successCallback(response) {
+                    if (response.data.token) {
+                        $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                        $localStorage.springWebUser = {username: $scope.reguser.username, token: response.data.token, isAdmin: response.data.isAdmin};
 
-                $scope.reguser = null;
+                        $scope.reguser = null;
 
-                $location.path('/');
-            }
-        });
+                        $location.path('/');
+                    }
+                }, function errorCallback(response) {
+                    if (response.status == 400) {
+                        alert(response.data.message);
+                    }
+            });
     }
 });
