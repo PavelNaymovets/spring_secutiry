aa7cea1617dd75eead874576d4d006428234f486
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-03-21 18:56:16 +0300

    Spring practice_hw_12. Add button for hide order list.

--- fr-service/src/main/resources/static/order/order.js
+++ fr-service/src/main/resources/static/order/order.js
@@ -1,15 +1,23 @@
 angular.module('market').controller('ordersController', function ($scope, $http) {
     const contextPath = 'http://localhost:8193/';
+    $scope.orderList = false;
 
     $scope.loadOrders = function (id = 1) {
         $http.get(contextPath + 'core/api/v1/orders')
             .then(function (response) {
                 $scope.orders = response.data;
-                $scope.orderId = id;
-
-                $scope.items = response.data[id - 1].items;
-                $scope.orderTotalPrice = response.data[id - 1].totalPrice;
         });
+    }
+
+    $scope.loadOrderProducts = function (id = 1) {
+        $scope.orderId = id;
+        $scope.items = $scope.orders[id - 1].items;
+        $scope.orderTotalPrice = $scope.orders[id - 1].totalPrice;
+        $scope.orderList = true;
+    }
+
+    $scope.hideOrderProducts = function () {
+        $scope.orderList = false;
     }
 
     $scope.loadOrders();
